59a0511e44f4ea12dac72a0c5ec2df8f1dbd1ebf
SUPATKN <yacht1400@gmail.com>
2023-01-13 16:06:05 +0700

    lab14_1 c

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,36 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n,double b[]){
+    double min = a[0], max = a[0];
+    double Num = n;
+    double sumX = 0;
+    double sumX2 = 0;
+    double powX=1;
+    double dividX=0;
+    for(int i=0 ; i<n ; i++){
+        sumX += a[i];
+        sumX2 += pow(a[i],2);
+        powX = powX*a[i];
+        dividX += 1/a[i];
+        if(a[i]>max){
+            max = a[i];
+        }
+        if(a[i]<min){
+            min = a[i];
+        }
+    }    
+        
+    double mean = sumX/n;
+    double Stand = sqrt((sumX2/n)-pow(mean,2));
+    double gm = pow(powX,1/Num);
+    double H = n/dividX;
+    b[0] = mean;
+    b[1] = Stand;
+    b[2] = gm;
+    b[3] = H;
+    b[4] = max;
+    b[5] = min;
+
+}
